cf58eb5002c0367aac963f6311b616bae47df840
ctarres <carles.tarres@ymbra.com>
2021-08-30 21:08:14 +0200

    Initial changes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+const fontFamily = defaultTheme.fontFamily;
+fontFamily['sans'] = [
+  '"Inter"',
+  'system-ui',
+  '-apple-system',
+  'BlinkMacSystemFont',
+  '"Segoe UI"',
+  'Roboto',
+  '"Helvetica Neue"',
+  'Arial',
+  '"Noto Sans"',
+  'sans-serif',
+  '"Apple Color Emoji"',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"',
+  '"Noto Color Emoji"',
+];
+
 module.exports = {
-  prefix: 'tw-',
-  important: true,
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
+    extend: {
+      fontFamily: fontFamily,
+    }
+  },
+  variants: {
     extend: {},
   },
-  variants: {},
-  plugins: []
+  plugins: [
+    require('daisyui'),
+  ],
 }
+
